6a8c48d7390c6cca283a38f9e97e5ea8a4c36571
Rogelio E. Cardona-Rivera <recardon@ncsu.edu>
2016-07-22 16:12:55 -0400

    Formatting the actions class.

--- actions.h
+++ actions.h
@@ -35,88 +35,88 @@ struct Bindings;
  * Abstract action definition.
  */
 struct Action {
-  /* Deletes this action. */
-  virtual ~Action();
+    /* Deletes this action. */
+    virtual ~Action();
 
-  /* Sets the condition for this action. */
-  void set_condition(const Formula& condition);
+    /* Sets the condition for this action. */
+    void set_condition(const Formula& condition);
 
-  /* Adds an effect to this action. */
-  void add_effect(const Effect& effect);
+    /* Adds an effect to this action. */
+    void add_effect(const Effect& effect);
 
-  /* Sets the minimum duration for this action. */
-  void set_min_duration(const Expression& min_duration);
+    /* Sets the minimum duration for this action. */
+    void set_min_duration(const Expression& min_duration);
 
-  /* Sets the maximum duration for this action. */
-  void set_max_duration(const Expression& max_duration);
+    /* Sets the maximum duration for this action. */
+    void set_max_duration(const Expression& max_duration);
 
-  /* Sets the duration for this action. */
-  void set_duration(const Expression& duration);
+    /* Sets the duration for this action. */
+    void set_duration(const Expression& duration);
 
-  /* Returns the id for this action. */
-  size_t id() const { return id_; }
+    /* Returns the id for this action. */
+    size_t id() const { return id_; }
 
-  /* Returns the name of this action. */
-  const std::string& name() const { return name_; }
+    /* Returns the name of this action. */
+    const std::string& name() const { return name_; }
 
-  /* Return the condition of this action. */
-  const Formula& condition() const { return *condition_; }
+    /* Return the condition of this action. */
+    const Formula& condition() const { return *condition_; }
 
-  /* List of action effects. */
-  const EffectList& effects() const { return effects_; }
+    /* List of action effects. */
+    const EffectList& effects() const { return effects_; }
 
-  /* Whether this is a durative action. */
-  bool durative() const { return durative_; }
+    /* Whether this is a durative action. */
+    bool durative() const { return durative_; }
 
-  /* Minimum duration of this action. */
-  const Expression& min_duration() const { return *min_duration_; }
+    /* Minimum duration of this action. */
+    const Expression& min_duration() const { return *min_duration_; }
 
-  /* Maximum duration of this action. */
-  const Expression& max_duration() const { return *max_duration_; }
+    /* Maximum duration of this action. */
+    const Expression& max_duration() const { return *max_duration_; }
 
-  /* "Strengthens" the effects of this action. */
-  void strengthen_effects(const Domain& domain);
+    /* "Strengthens" the effects of this action. */
+    void strengthen_effects(const Domain& domain);
 
-  /* Prints this action on the given stream with the given bindings. */
-  virtual void print(std::ostream& os,
-		     size_t step_id, const Bindings& bindings) const = 0;
+    /* Prints this action on the given stream with the given bindings. */
+    virtual void print(std::ostream& os,
+        size_t step_id, const Bindings& bindings) const = 0;
 
 protected:
-  /* Constructs an action with the given name. */
-  Action(const std::string& name, bool durative);
+    /* Constructs an action with the given name. */
+    Action(const std::string& name, bool durative);
 
 private:
-  /* Next action id. */
-  static size_t next_id;
+    /* Next action id. */
+    static size_t next_id;
 
-  /* Unique id for actions. */
-  size_t id_;
-  /* Name of this action. */
-  std::string name_;
-  /* Action condition. */
-  const Formula* condition_;
-  /* List of action effects. */
-  EffectList effects_;
-  /* Whether this is a durative action. */
-  bool durative_;
-  /* Minimum duration of this action. */
-  const Expression* min_duration_;
-  /* Maximum duration of this action. */
-  const Expression* max_duration_;
+    /* Unique id for actions. */
+    size_t id_;
+    /* Name of this action. */
+    std::string name_;
+    /* Action condition. */
+    const Formula* condition_;
+    /* List of action effects. */
+    EffectList effects_;
+    /* Whether this is a durative action. */
+    bool durative_;
+    /* Minimum duration of this action. */
+    const Expression* min_duration_;
+    /* Maximum duration of this action. */
+    const Expression* max_duration_;
 };
 
 /*
  * Less than function object for action pointers.
  */
 namespace std {
-  template<>
-  struct less<const Action*>
-    : public binary_function<const Action*, const Action*, bool> {
-    /* Comparison function operator. */
-    bool operator()(const Action* a1, const Action* a2) const {
-      return a1->id() < a2->id();
-    }
-  };
+    template<>
+    struct less<const Action*>
+        : public binary_function < const Action*, const Action*, bool > {
+        /* Comparison function operator. */
+        bool operator()(const Action* a1, const Action* a2) const {
+            return a1->id() < a2->id();
+        }
+    };
 }
 
 
@@ -126,7 +126,7 @@ namespace std {
 /*
  * List of action definitions.
  */
-struct ActionList : public std::vector<const Action*> {
+struct ActionList : public std::vector < const Action* > {
 };
 
 
@@ -140,43 +140,43 @@ struct GroundActionList;
  * Action schema definition.
  */
 struct ActionSchema : public Action {
-  /* Constructs an action schema with the given name. */
-  ActionSchema(const std::string& name, bool durative);
+    /* Constructs an action schema with the given name. */
+    ActionSchema(const std::string& name, bool durative);
 
-  /* Adds a parameter to this action schema. */
-  void add_parameter(Variable var);
+    /* Adds a parameter to this action schema. */
+    void add_parameter(Variable var);
 
-  /* Returns the parameters of this action schema. */
-  const VariableList& parameters() const { return parameters_; }
+    /* Returns the parameters of this action schema. */
+    const VariableList& parameters() const { return parameters_; }
 
-  /* Fills the provided action list with all instantiations of this
-     action schema. */
-  void instantiations(GroundActionList& actions, const Problem& problem) const;
+    /* Fills the provided action list with all instantiations of this
+       action schema. */
+    void instantiations(GroundActionList& actions, const Problem& problem) const;
 
-  /* Prints this action on the given stream. */
-  void print(std::ostream& os) const;
+    /* Prints this action on the given stream. */
+    void print(std::ostream& os) const;
 
-  /* Prints this action on the given stream with the given bindings. */
-  virtual void print(std::ostream& os,
-		     size_t step_id, const Bindings& bindings) const;
+    /* Prints this action on the given stream with the given bindings. */
+    virtual void print(std::ostream& os,
+        size_t step_id, const Bindings& bindings) const;
 
 private:
-  /* Action schema parameters. */
-  VariableList parameters_;
+    /* Action schema parameters. */
+    VariableList parameters_;
 
-  /* Returns an instantiation of this action schema. */
-  const GroundAction* instantiation(const SubstitutionMap& args,
-				    const Problem& problem,
-				    const Formula& condition) const;
+    /* Returns an instantiation of this action schema. */
+    const GroundAction* instantiation(const SubstitutionMap& args,
+        const Problem& problem,
+        const Formula& condition) const;
 };
 
 /*
  * Less than function object for action schemas pointers.
  */
 namespace std {
-  template<>
-  struct less<const ActionSchema*> : public less<const Action*> {
-  };
+    template<>
+    struct less<const ActionSchema*> : public less < const Action* > {
+    };
 }
 
 
@@ -186,7 +186,7 @@ namespace std {
 /*
  * Table of action schema definitions.
  */
-struct ActionSchemaMap : public std::map<std::string, const ActionSchema*> {
+struct ActionSchemaMap : public std::map < std::string, const ActionSchema* > {
 };
 
 
@@ -197,31 +197,31 @@ struct ActionSchemaMap : public std::map<std::string, const ActionSchema*> {
  * Ground action.
  */
 struct GroundAction : public Action {
-  /* Constructs a ground action with the given name. */
-  GroundAction(const std::string& name, bool durative);
+    /* Constructs a ground action with the given name. */
+    GroundAction(const std::string& name, bool durative);
 
-  /* Adds an argument to this ground action. */
-  void add_argument(Object arg);
+    /* Adds an argument to this ground action. */
+    void add_argument(Object arg);
 
-  /* Action arguments. */
-  const ObjectList& arguments() const { return arguments_; }
+    /* Action arguments. */
+    const ObjectList& arguments() const { return arguments_; }
 
-  /* Prints this action on the given stream with the given bindings. */
-  virtual void print(std::ostream& os,
-		     size_t step_id, const Bindings& bindings) const;
+    /* Prints this action on the given stream with the given bindings. */
+    virtual void print(std::ostream& os,
+        size_t step_id, const Bindings& bindings) const;
 
 private:
-  /* Action arguments. */
-  ObjectList arguments_;
+    /* Action arguments. */
+    ObjectList arguments_;
 };
 
 /*
  * Less than function object for ground action pointers.
  */
 namespace std {
-  template<>
-  struct less<const GroundAction*> : public less<const Action*> {
-  };
+    template<>
+    struct less<const GroundAction*> : public less < const Action* > {
+    };
 }
 
 
@@ -231,7 +231,7 @@ namespace std {
 /*
  * List of ground actions.
  */
-struct GroundActionList : public std::vector<const GroundAction*> {
+struct GroundActionList : public std::vector < const GroundAction* > {
 };
 
 
