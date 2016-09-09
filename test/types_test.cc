03af2ad348952b29306efcedd6ed8ba4f0a997c0
Rogelio E. Cardona-Rivera <recardon@ncsu.edu>
2016-09-09 15:47:46 -0400

    Developed tests for the object hierarchy construction.

--- test/types_test.cc
+++ test/types_test.cc
@@ -5,6 +5,7 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+
 namespace test
 {
     /*
@@ -64,9 +65,48 @@ namespace test
             Assert::AreEqual(expected, number_name, L"The top level number type should be called 'type'");
         }
 
+        TEST_METHOD(TestObjectHierarchy)
+        {
+            read_file("..\\test\\travel_domain.pddl");
+            read_file("..\\test\\travel_problem.pddl");
+            const Domain* domain = Domain::find("travel");
+            const Problem* problem = Problem::find("travel-to-la");
+
+
+            // Check the typing hierarchy
+            TypeTable tt = domain->types();
+            Type object = TypeTable::OBJECT;
+            const Type* place = tt.find_type("place");
+            const Type* transport = tt.find_type("transport");
+            const Type* person = tt.find_type("person");
+            const Type* plane = tt.find_type("plane");
+            const Type* car = tt.find_type("car");
+
+            // All types are subtypes of object
+            Assert::IsTrue(TypeTable::subtype(*place, object));
+            Assert::IsTrue(TypeTable::subtype(*transport, object));
+            Assert::IsTrue(TypeTable::subtype(*person, object));
+            Assert::IsTrue(TypeTable::subtype(*plane, object));
+            Assert::IsTrue(TypeTable::subtype(*car, object));
+
+            // Object is not a subtype of anything
+            Assert::IsFalse(TypeTable::subtype(object, *place));
+            Assert::IsFalse(TypeTable::subtype(object, *transport));
+            Assert::IsFalse(TypeTable::subtype(object, *person));
+            Assert::IsFalse(TypeTable::subtype(object, *plane));
+            Assert::IsFalse(TypeTable::subtype(object, *car));
+
+
+            // Plane and Car are subtypes of Transport
+            Assert::IsTrue(TypeTable::subtype(*plane, *transport));
+            Assert::IsTrue(TypeTable::subtype(*car, *transport));
+
+            // Transport is not a subtype of Plane or Car
+            Assert::IsFalse(TypeTable::subtype(*transport, *plane));
+            Assert::IsFalse(TypeTable::subtype(*transport, *car));
+
+
+        }
+
     };
-
-
-
-
 }
