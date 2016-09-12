8a79e6d20d4c2a8fef4e68a88a60440876ef55f2
Rogelio E. Cardona-Rivera <recardon@ncsu.edu>
2016-09-12 19:07:16 -0400

    Adding stubs for testing code.

---
+++ test/decomp_plan_test.cc
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace test
+{
+	TEST_CLASS(DecompositionPlanTest)
+	{
+	public:
+		
+		TEST_METHOD(TestDecompositionalPlanning)
+		{
+            // Setup
+            read_file("..\\test\\travel_domain.pddl");
+            read_file("..\\test\\travel_problem.pddl");
+            const Domain* domain = Domain::find("travel");
+            const Problem* problem = Problem::find("travel-to-la");
+            const Parameters* params = new Parameters();
+
+            // Find a plan
+            // This currently blows up->
+            // const Plan* plan = Plan::plan(*problem, *params, true);
+
+            // TODO: UPDATE
+            // For now, this plan should exist, and it should have a non-zero 
+            // number of unexpanded composite step flaws.
+            // Assert::IsNull(plan, L"There should not be a plan that solves this problem.");
+
+
+
+		}
+
+	};
+}
