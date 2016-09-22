1961ec78e4064a28b0ff8b65165c46b9963f40c5
Rogelio E. Cardona-Rivera <recardon@ncsu.edu>
2016-09-22 16:51:30 -0400

    Testing the Longbow* ordering.

--- test/decomp_plan_test.cc
+++ test/decomp_plan_test.cc
@@ -16,11 +16,15 @@ namespace test
             read_file("..\\test\\travel_problem.pddl");
             const Domain* domain = Domain::find("travel");
             const Problem* problem = Problem::find("travel-to-la");
-            const Parameters* params = new Parameters();
+            Parameters params = Parameters();
+
+            std::vector<FlawSelectionOrder> orders;
+            orders.push_back(FlawSelectionOrder("Longbow*"));
+            params.flaw_orders = orders;
 
             // Find a plan
             // This currently blows up->
-            // const Plan* plan = Plan::plan(*problem, *params, true);
+            const Plan* plan = Plan::plan(*problem, params, true);
 
             // TODO: UPDATE
             // For now, this plan should exist, and it should have a non-zero 
