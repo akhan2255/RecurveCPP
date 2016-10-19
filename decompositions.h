a39d2db4101dbb7839893210651e4ab345bcf25d
Rogelio E. Cardona-Rivera <recardon@ncsu.edu>
2016-10-19 11:14:39 -0400

    Moved the DecompositionFrame and DecompositionLink to the decompositions header.
    It makes more sense to me to group these structs by the fact that they all relate to the decompositions extension.

--- decompositions.h
+++ decompositions.h
@@ -136,6 +136,106 @@ private:
 
 
 /* ====================================================================== */
+/* DecompositionFrame */
+
+/* Represents an instantiated decomposition. It's the decompositional analogue to
+the basic plan step. */
+struct DecompositionFrame {
+
+    /* Constructs a decomposition step instantiated from a decomposition. */
+    DecompositionFrame(int id, const Decomposition& decomposition)
+    : id_(id),
+    decomposition_(&decomposition),
+    steps_(decomposition.pseudo_steps()),
+    binding_list_(decomposition.binding_list()),
+    ordering_list_(decomposition.ordering_list()),
+    link_list_(decomposition.link_list()) {}
+
+    /* Constructs a decomposition step. */
+    DecompositionFrame(const DecompositionFrame& ds)
+        : id_(ds.id_),
+        decomposition_(ds.decomposition_),
+        steps_(ds.steps_),
+        binding_list_(ds.binding_list()),
+        ordering_list_(ds.ordering_list()),
+        link_list_(ds.link_list()) {}
+
+    /* Returns the decomposition step id. */
+    int id() const { return id_; }
+
+    /* Returns the decomposition that this step is instantiated from. */
+    const Decomposition& decomposition() const { return *decomposition_; }
+
+    /* Returns the steps that belong to this decomposition. */
+    const StepList steps() const { return steps_; }
+
+    /* Returns the list of bindings for this decomposition. */
+    const BindingList binding_list() const { return binding_list_; }
+
+    /* Returns the list of ordering constraints of this decomposition. */
+    const OrderingList ordering_list() const { return ordering_list_; }
+
+    /* Returns the list of causal links of this decomposition. */
+    const LinkList link_list() const { return link_list_; }
+
+private:
+
+    /* Decomposition step id */
+    int id_;
+
+    /* Decomposition that this decomposition step is instantiated from. */
+    const Decomposition* decomposition_;
+
+    /* List of steps that belong to this decomposition. */
+    StepList steps_;
+
+    /* List of bindings for this decomposition. */
+    BindingList binding_list_;
+
+    /* List of ordering constraints of this decomposition. */
+    OrderingList ordering_list_;
+
+    /* List of causal links of this decomposition. */
+    LinkList link_list_;
+};
+
+
+/* ====================================================================== */
+/* Decomposition Link */
+
+/*
+* A DecompositionLink is used to record the fact that the purpose of some
+* step s is to be part of a more-primitive realization of some other step.
+*/
+struct DecompositionLink {
+
+    /* Constructs a decomposition link. */
+    DecompositionLink(int composite_id, DecompositionFrame& decomposition_frame)
+    : composite_id_(composite_id), decomposition_frame_(decomposition_frame) {}
+
+    /* Returns the id of the composite step being decomposed. */
+    int composite_id() const { return composite_id_; }
+
+    /* Returns the decomposition step that refines the composite step of this decomposition link. */
+    const DecompositionFrame decomposition_frame() const { return decomposition_frame_; }
+
+private:
+
+    /* Id of the composite step being decomposed. */
+    int composite_id_;
+
+    /* The decomposition step that refines the composite step identified by the id. */
+    DecompositionFrame decomposition_frame_;
+
+};
+
+/* Equality operator for decomposition links. */
+inline bool operator==(const DecompositionLink& l1, const DecompositionLink& l2) {
+    return &l1 == &l2;
+}
+
+
+/* ====================================================================== */
 /* DecompositionSchemaMap */
 
 /* Table of decomposition schema definitions. This table maps a pair of strings to a decomposition
