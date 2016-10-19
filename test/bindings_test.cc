469f1efe67c88760966f3cfae91811cae02c3eba
Rogelio E. Cardona-Rivera <recardon@ncsu.edu>
2016-10-19 11:43:08 -0400

    Renaming .cpp files to .cc files for consistency (also adding a new bindings_test.cc)

---
+++ test/bindings_test.cc
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace test
+{
+	TEST_CLASS(bindings_test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
